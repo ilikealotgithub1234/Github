9e6ed01b750cbc3dd1fb52ef80dbbab290f9a0e4
ilikegithub123 <erikpmurdock@gmail.com>
2016-03-17 20:46:22 -0400

    commiting the first commit

---
+++ Week3/uploadData/.js
@@ -0,0 +1,6 @@
+var $dataButton = $('#data');
+var doto(jQureyObj,function){
+  jQueryObj.on('click',function)
+
+}
+var apis = {'weather':{'url':'http://api.openweathermap.org/data/2.5/weather?q=${New York}&appid=${d9ff9547a8c6475c6fb3de92b53235e8}&units=imperial'}'apiKey''d9ff9547a8c6475c6fb3de92b53235e8':}
